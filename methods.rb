ba4ac4605c461693aba0f4ee126f34d8e92e2286
bobertsworld <bobertsworld@gmail.com>
2023-01-10 22:11:35 -0800

    commit tree 05c498beb464fd1bd10d744bf7e73e0685a4e087

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+# def greet_Programmer(name)
+#   puts "Hi there, #{name}!"
+# end
+def greet_programmer()
+  puts "Hello, programmer!"
+end
+
+
+def greet(name = Naureen)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "Naureen")
+  puts "Hello, #{name}!"
+end
+# greet_with_default "Naureen"
+
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+
+def halve(number)
+  if (number.class != Integer) 
+    return nil;
+  end
+    return number / 2;
+  end
+
